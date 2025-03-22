1e3abf3afd6ae44a71e15d539209cc8642988df4
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-22 12:50:40 -0300

    feat: exemplo de como gerar e utilizar jwt

--- example_jwt.py
+++ example_jwt.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 import jwt
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 def login():
     token = jwt.encode(
         payload={
-            "exp": datetime.now(timezone.utc) + timedelta(minutes=1)
+            "exp": datetime.now(timezone.utc) + timedelta(minutes=1),
+            "email": "andreza@a.com"
         },
         key="minhaChave",
         algorithm="HS256"
@@ -18,7 +19,16 @@ def login():
 
 @app.route("/secret", methods=["POST"])
 def secret():
+    raw_token = request.headers.get("Authorization")
+    token = raw_token.split()[1]
+    # print(token)
+    try:
+        token_information = jwt.decode(token, key="minhaChave", algorithms="HS256")
+        print(token_information)
+        print(token_information["email"])
+    except Exception as exception:
+        return jsonify({"error": str(exception)}), 400
     return jsonify({"meu": "segredo"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000, debug=True)
