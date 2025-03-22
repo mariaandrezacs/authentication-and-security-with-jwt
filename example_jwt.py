f15090ebc9df537e3afcf700b55cfc7488f4fe20
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-21 22:20:46 -0300

    feat: adicionando exemplos

---
+++ example_jwt.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def login():
+    token = jwt.encode(
+        payload={
+            "exp": datetime.now(timezone.utc) + timedelta(minutes=1)
+        },
+        key="minhaChave",
+        algorithm="HS256"
+    )
+    return jsonify({"token": token}), 200
+
+@app.route("/secret", methods=["POST"])
+def secret():
+    return jsonify({"meu": "segredo"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
