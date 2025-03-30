ff5310dec0381350f2ca2fa1050997d4ed08e53b
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-30 13:35:21 -0300

    feat: implamenta UserRepositoryInterface

---
+++ src/controllers/user_register.py
@@ -0,0 +1,5 @@
+
+
+class UserRegister:
+    def __init__(self, user_repository) -> None:
+        pass
--- src/models/repositories/user_repository.py
+++ src/models/repositories/user_repository.py
@@ -1,9 +1,10 @@
 # pylint: disable=missing-class-docstring
 
 from sqlite3 import Connection
+from ..interface.user_repository import UserRepositoryInterface
 
 
-class UserRepository:
+class UserRepository(UserRepositoryInterface):
     def __init__(self, conn: Connection) -> None:
         self.__conn = conn
 
@@ -30,7 +31,7 @@ class UserRepository:
         self.__conn.commit()
 
 
-    def get_user_by_username(self, username: str):
+    def get_user_by_username(self, username: str) -> tuple[int, str, str]:
         cursor = self.__conn.cursor()
         cursor.execute(
             '''
